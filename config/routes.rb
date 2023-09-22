60d6eeefbb4f81ed952688b04e256f094a0b0e92
daitomoyumiharu <sakanashi0814@gmail.com>
2023-09-22 20:28:52 +0900

    indexへのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'musics/index'
+  root to: "items#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
