268998a51fda012c564422a33a6e546135aecdac
daitomoyumiharu <sakanashi0814@gmail.com>
2023-09-27 09:23:57 +0900

    deviceのDB修正

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_25_000523) do
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "last_name", null: false
