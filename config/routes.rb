4fb0f54122bb3dcc118698303a6f7e540e2e3a83
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-09 19:18:28 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to:"musics#index"
 
-  resources :musics, only: [:index, :new, :create, :show] do
+  resources :musics, only: [:index, :new, :create, :show, :edit] do
     resources :comments, only: :create
   end
   
