0ad605d829516e7d41745d41ada829684e20f4eb
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-09 19:50:15 +0900

    共通するアクション内容をset_musicに設定

--- app/controllers/musics_controller.rb
+++ app/controllers/musics_controller.rb
@@ -1,6 +1,7 @@
 class MusicsController < ApplicationController
   #ログアウト状態のユーザーが投稿ページへ遷移を試みると、ログインページへリダイレクトするよう設定
   before_action :authenticate_user!, except: [:index]
+  before_action :set_music, only: [:show, :edit]
 
 
   
@@ -25,21 +26,25 @@ class MusicsController < ApplicationController
   end
 
   def show
-    @music = Music.find(params[:id])
     @comment = Comment.new
     @comments = @music.comments.includes(:user).order(created_at: :desc)
   end
 
   def edit
     # ログインしているユーザー且つ編集ページのユーザーと一致していればeditファイルが読み込まれる
-    if @music.user_id == current_user
+    if @music.user_id == current_user.id
     else
       redirect_to root_path
     end
   end
+  
 
   def music_params
     params.require(:music).permit(:game_name, :title, :description, :music_file, :year_id, :genre_id, :target, :image)
+  end
+
+  def set_item
+    @music = Music.find(params[:id])
   end
 
 end
