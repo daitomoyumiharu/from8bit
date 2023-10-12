adaba38c80a518ad82b6d225637db282d13dd182
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-12 09:09:35 +0900

    コントローラーにindex及びshowアクションの設定

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -1,2 +1,11 @@
 class GenresController < ApplicationController
+  def index
+    @genres = Genre.all
+  end
+
+  def show
+    @genre = Genre.find(params[:id])
+    @musics = Music.where(genre_id: @genre.id)
+  end
+
 end
