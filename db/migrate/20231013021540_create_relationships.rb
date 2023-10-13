d769398bceb3ad4b449250ff6c734c52f695ac81
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-13 12:00:37 +0900

    テーブル名の追加

--- db/migrate/20231013021540_create_relationships.rb
+++ db/migrate/20231013021540_create_relationships.rb
@@ -1,6 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[7.0]
   def change
     create_table :relationships do |t|
+      t.references :following
+      t.references :follower
 
       t.timestamps
     end
