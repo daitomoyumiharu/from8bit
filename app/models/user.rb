04a76cb0c22018614c8ce8e6be067f139694c941
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-14 07:53:28 +0900

    フォローボタンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,7 @@ class User < ApplicationRecord
   has_many :followers, through: :passive_relationships, source: :following
 
   def followed_by?(user)
-    follower = passive_relationships.find__by(following_id: user.id)
+    follower = passive_relationships.find_by(following_id: user.id)
     return follower.present?
   end
 
