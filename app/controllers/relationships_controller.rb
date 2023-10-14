2923e3990d6814fb9cbb83d965578f659a038ffd
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-15 08:57:07 +0900

    createアクション定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,7 @@
 class RelationshipsController < ApplicationController
+  def create
+    follow = current_user.active_relationships.new(follwer_id: params[:user_id])
+    follow.save
+    redirect_to root_path
+  end
 end
