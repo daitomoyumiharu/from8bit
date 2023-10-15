8bf191d00a8e0c99b141fad93732c2a44c9bef2a
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-15 09:05:46 +0900

    フォロー解除機能（destroyアクション）の定義

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,4 +4,11 @@ class RelationshipsController < ApplicationController
     follow.save
     redirect_to root_path
   end
+  
+  def destroy
+    follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
+    follow.destroy
+    redirect_to root_path
+  end
+
 end
