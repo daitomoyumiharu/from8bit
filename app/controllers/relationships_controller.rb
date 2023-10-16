a47233439242ff4d0c2fcfb905e989634361322a
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-16 09:02:09 +0900

    フォローボタン表示ページの修正

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,6 +1,6 @@
 class RelationshipsController < ApplicationController
   def create
-    follow = current_user.active_relationships.new(follwer_id: params[:user_id])
+    follow = current_user.active_relationships.new(follower_id: params[:user_id])
     follow.save
     redirect_to root_path
   end
