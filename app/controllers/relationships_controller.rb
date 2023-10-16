31c1d9796fe3898b6d6837a7dfb82853f8f7d913
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-16 10:06:01 +0900

    フォロー後、詳細画面（直前の画面）に遷移するよう変更

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,13 +2,13 @@ class RelationshipsController < ApplicationController
   def create
     follow = current_user.active_relationships.new(follower_id: params[:user_id])
     follow.save
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
   
   def destroy
     follow = current_user.active_relationships.find_by(follower_id: params[:user_id])
     follow.destroy
-    redirect_to root_path
+    redirect_back(fallback_location: root_path)
   end
 
 end
