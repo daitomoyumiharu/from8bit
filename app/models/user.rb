6b071aed35ed744bff4fd793ff10433fd1e0ef07
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-18 06:53:55 +0900

    Userモデルに関連付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ApplicationRecord
   has_many :musics
   has_many :comments
   has_many :likes
+  has_many :liked_music, through: :likes, source: :music
 
   has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
   has_many :followings, through: :active_relationships, source: :follower
