a938828dc41150cd26ff282a41b5a5b120be96f1
daitomoyumiharu <sakanashi0814@gmail.com>
2023-10-17 16:39:21 +0900

    いいね機能の非同期化

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,13 +1,22 @@
 class LikesController < ApplicationController
+  before_action :set_music
   def create
     like = current_user.likes.build(music_id: params[:music_id])
     like.save
-    redirect_to music_path(params[:music_id])
+    respond_to do |format|
+      format.js
+    end
   end
 
   def destroy
-    like = Like.find_by(music_id: params[:music_id], user_id: current_user.id)
+    like = current_user.likes.find_by(music_id: @music.id)
     like.destroy
-    redirect_to music_path(params[:music_id])
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  def set_music
+    @music = Music.find(params[:music_id])
   end
 end
